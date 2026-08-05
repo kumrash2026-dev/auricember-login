db84afb8a1f97d0958e76f8e38f1783c46b13033
kumrash2026-dev <kumrash2026@gmail.com>
2026-08-06 05:07:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import {initializeApp} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,sendPasswordResetEmail} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig={apiKey:"AIzaSyBdqq63eP_zc6UWjc5sSVRdwjoUnUzPYT4",authDomain:"auricember.firebaseapp.com",projectId:"auricember",storageBucket:"auricember.firebasestorage.app",messagingSenderId:"568693145265",appId:"1:568693145265:web:de258d43d06ff35778ec90",measurementId:"G-E109YXWP1W"};
-const app=initializeApp(firebaseConfig);const auth=getAuth(app);const e=()=>email.value,p=()=>password.value,m=document.getElementById('msg');
+const app=initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth=getAuth(app);const e=()=>email.value,p=()=>password.value,m=document.getElementById('msg');
 signup.onclick=()=>createUserWithEmailAndPassword(auth,e(),p()).then(()=>m.textContent='Account created').catch(x=>m.textContent=x.message);
 login.onclick = () =>
   signInWithEmailAndPassword(auth, e(), p())
