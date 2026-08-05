4c479d6aab6d970c551aed9609ec61e9c04ebf7b
kumrash2026-dev <kumrash2026@gmail.com>
2026-08-06 03:54:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,sendPa
 const firebaseConfig={apiKey:"AIzaSyBdqq63eP_zc6UWjc5sSVRdwjoUnUzPYT4",authDomain:"auricember.firebaseapp.com",projectId:"auricember",storageBucket:"auricember.firebasestorage.app",messagingSenderId:"568693145265",appId:"1:568693145265:web:de258d43d06ff35778ec90",measurementId:"G-E109YXWP1W"};
 const app=initializeApp(firebaseConfig);const auth=getAuth(app);const e=()=>email.value,p=()=>password.value,m=document.getElementById('msg');
 signup.onclick=()=>createUserWithEmailAndPassword(auth,e(),p()).then(()=>m.textContent='Account created').catch(x=>m.textContent=x.message);
-login.onclick=()=>signInWithEmailAndPassword(auth,e(),p()).then(()=>m.textContent = "Login successful...";
+login.onclick = () =>
+  signInWithEmailAndPassword(auth, e(), p())
+    .then(() => {
+      m.textContent = "Login successful...";
+      setTimeout(() => {
+        window.location.href = "dashboard.html";
+      }, 1000);
+    })
+    .catch(x => {
+      m.textContent = x.message;
+    });
 setTimeout(() => {
     window.location.href = "dashboard.html";
 }, 1000)).catch(x=>m.textContent=x.message);
